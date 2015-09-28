676f6be96a10e7e82960f566dc6fc7efc2199633
Crigges <andy400@web.de>
2015-09-28 16:22:03 +0200

    Added package generator

--- src/de/crigges/main/Main.java
+++ src/de/crigges/main/Main.java
@@ -2,6 +2,8 @@ package de.crigges.main;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.NoSuchElementException;
 
 import de.crigges.main.SylkParser.Entry;
 
@@ -10,13 +12,20 @@ public class Main {
 	public static void main(String[] args) {
 		try {
 			SylkParser p = new SylkParser(new File("C:\\Users\\Crigges-Pc\\Desktop\\unitui.slk"), 1, 3);
-			UnitStringParser par = new UnitStringParser(new File("C:\\Users\\Crigges-Pc\\Desktop\\unitstrings\\comb.txt"));
-			while(true){
-				Entry e = p.next();
-				String name = par.find(e.id);
-				System.out.println("Path: " + e.path + "  |  Name: " + name);
+			UnitStringParser par = new UnitStringParser(new File("C:\\Users\\Crigges-Pc\\Desktop\\unitstringseng\\comb.txt"));
+			ConstantList list = new ConstantList();
+			try{
+				while(true){
+					Entry e = p.next();
+					String name = par.find(e.id);
+					list.add(e.path, name);
+				}
+			}catch(NoSuchElementException e){
+				PackageGenerator gen = new PackageGenerator(new File("Assets.wurst"), "Assets");
+				list.writeToPackage(gen);
+				gen.close();
 			}
-		} catch (FileNotFoundException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
